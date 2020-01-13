d651ed6b3f562bd93186a7c71328ea8c1da96daa
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-01-13 21:20:36 +0530

    component scan in xml configuration in applicationContent.xml

--- spring-in-5-steps/src/main/java/com/gawari/_himanshu/springin5steps/SpringIn5StepsXmlContextApplication.java
+++ spring-in-5-steps/src/main/java/com/gawari/_himanshu/springin5steps/SpringIn5StepsXmlContextApplication.java
@@ -1,15 +1,24 @@
 package com.gawari._himanshu.springin5steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.gawari._himanshu.springin5steps.xml.XMLPersonDao;
 
 public class SpringIn5StepsXmlContextApplication {
 
+	//private static Logger LOGGER = (Logger) LoggerFactory.getLogger(SpringIn5StepsXmlContextApplication.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsXmlContextApplication.class);
+
 	public static void main(String[] args) {
 
 		try (ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
 				"ApplicationContext.xml")) {
+
+			LOGGER.info("Bean Names -> {}",(Object)classPathXmlApplicationContext.getBeanDefinitionNames());
+			LOGGER.info("Bean Count -> {}",(Object)classPathXmlApplicationContext.getBeanDefinitionCount());
+
 			XMLPersonDao xMLPersonDao = classPathXmlApplicationContext.getBean(XMLPersonDao.class);
 			System.out.println(xMLPersonDao);
 			System.out.println(xMLPersonDao.getxMLJdbcConnection());
