ed80636c05484e2d4720885ab16d99b57f53e29f
Himanshu Gawari <himanshugawarigit@gmail.com>
2020-01-15 19:03:43 +0530

    initial commit

---
+++ spring-aop/src/test/java/com/gawari/_himanshu/spring/aop/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gawari._himanshu.spring.aop.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
